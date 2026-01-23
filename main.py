e765df2320d1e017650a3ebe32002da3e25b85e4
josephor1271 <josephor@mit.edu>
2026-01-23 14:48:05 -0500

    .

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 import rkhs_ls_pipeline as RKHS
 
-# RKHS.nonlocal_linwise_reg_ls("gogh_self.jpg", num_strips=2, strip_width=(1,1), t= 0.5, p= 0.5, gamma = 0.0001)
-
 RKHS.nonlocal_linwise_reg_ls(
-    "starry_night_small.jpg",
-    num_strips=5,
-    strip_width=(1, 2),
-    t=0.001,
-    p=2,
-    gamma=0.0001,
+    "gogh_self.jpg", num_strips=2, strip_width=(1, 1), t=0.5, p=0.5, gamma=0.0001
 )
-
-# RKHS.nonlocal_linwise_reg_ls(
-#     "rainbow_face.jpg",
-#     num_strips=5,
-#     strip_width=(1, 3),
-#     t=0.001,
-#     p=2,
-#     gamma=0.0001,
-# )
