f5f79de705ad1fe5440734b39f724684c71d7495
josephor1271 <josephor@mit.edu>
2026-01-24 14:57:33 -0500

    photos

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import rkhs_ls_pipeline as RKHS
+import rkhs_recoloring as K
 
 # RKHS.nonlocal_linwise_reg_ls("gogh_self.jpg", num_strips=2, strip_width=(1,1), t= 0.5, p= 0.5, gamma = 0.0001)
 
-RKHS.nonlocal_linwise_reg_ls(
+# RKHS.nonlocal_linwise_reg_ls(
+#     "starry_night_small.jpg",
+#     num_strips=3,
+#     strip_width=(1, 2),
+#     t=0.001,
+#     p=2,
+#     gamma=0.0001,
+# )
+
+
+RKHS.mix_kernel_linwise_reg_ls(
     "starry_night_small.jpg",
-    num_strips=5,
-    strip_width=(1, 2),
+    num_strips=7,
+    strip_width=(1, 1),
     t=0.001,
     p=2,
-    gamma=0.0001,
+    gamma=0.001,
 )
-
 # RKHS.nonlocal_linwise_reg_ls(
 #     "rainbow_face.jpg",
 #     num_strips=5,
