d2274bae1a918e243244434c5589d39657f761ad
josephor1271 <josephor@mit.edu>
2026-01-30 14:21:48 -0500

    more testing and production

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import rkhs_ls_pipeline as RKHS
 import kernel_factory as Kf
 import pixel_sampling as Ps
 
-local_k = Kf.make_local_kernel_factory(t=4, p=1)
-nonlocal_k = Kf.make_nonlocal_kernel_factory(t=4, p=1)
+local_k = Kf.make_local_kernel_factory(t=0.04, p=1)
+nonlocal_k = Kf.make_nonlocal_kernel_factory(t=0.05, p=1.9)
 mixed_k = Kf.combine_kernel_factories(nonlocal_k, local_k, 0.05, 0.001)
 
-sample_strips = Ps.sample_random_strips(5, width_range=(1, 2))
+sample_strips = Ps.sample_random_strips(
+    2, width_range=(2, 2), orientation="vertical", rng=1
+)
 sample_blotches = Ps.sample_random_blotches(10, radius_range=(5, 7))
-sample_pix = Ps.sample_random_pixels(100, rng=1)
+sample_pix = Ps.sample_random_pixels(550)
 
 RKHS.recolor(
-    dest="outputs/bolt.jpg",
-    dataset_src="bolt.jpg",
+    dest="outputs/starry_night.jpg",
+    dataset_src="starry_night.jpg",
     make_k=mixed_k,
     sample_function=sample_pix,
-    gamma=1e-4,
+    gamma=3e-6,
 )
